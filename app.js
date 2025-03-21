f299e73a54ede4811ffe268fb2fd81e723a2ca88
Ammar Hassan <ammar@ammarhassan.co.uk>
2025-03-21 04:47:45 +0000

    objection creation as explained in step1 of markup file

---
+++ app.js
@@ -0,0 +1,29 @@
+const library = [
+    {
+        title: 'Indstractable',
+        author: 'Nir Eyal',
+        status: {
+            own: false,
+            reading: true,
+            read: false
+        }
+    },
+    {
+        title: 'What\'s your dream?',
+        author: 'Simon squibb',
+        status: {
+            own: false,
+            reading: true,
+            read: false
+        }
+    },
+    {
+        title: 'Diary of CEO',
+        author: 'Steven Barlett',
+        status: {
+            own: false,
+            reading: true,
+            read: false
+        }
+    }
+]
