f74ae53c47f9e4d26264fedee80b8ae3251839cc
Ammar Hassan <ammar.hassan@group1uk.group1auto.com>
2025-03-26 09:58:07 +0000

    Finding even number using filter

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+// function findEven(arr){
+//  arr.forEach(num =>{
+//    if(num % 2 === 0){
+//      console.log(`${num} is even`)
+//    }
+//  })
+// }
 function findEven(arr){
- arr.forEach(num =>{
-   if(num % 2 === 0){
-     console.log(`${num} is even`)
-   }
- })
+    arr.filter((num)=>{
+        if(num % 2 === 0){
+            console.log(num);
+        }
+    })
 }
-
 const arrays = [1,2,3,4,5,6,7,8,9,10];
 
 findEven(arrays)
